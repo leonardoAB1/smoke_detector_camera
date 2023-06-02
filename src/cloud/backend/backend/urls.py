14b5bc46978818181d9de7e6a4989e3982bfb558
Leonardo Achá <leonardoacha@hotmail.com>
2023-06-01 23:44:53 -0400

    web directories folder name update

--- src/cloud/backend/backend/urls.py
+++ src/cloud/backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""smoke_detector_backend URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
