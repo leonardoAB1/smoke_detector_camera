56f3857a0db0df4e70be9f97b340fb002085f515
Leonardo Achá <leonardoacha@hotmail.com>
2023-06-07 23:46:31 -0400

    logging_utils.h file documentation improved

--- src/device/main/src/logging/logging_utils.h
+++ src/device/main/src/logging/logging_utils.h
@@ -1,20 +1,31 @@
 /*******************************************************************************
  * @file        logging_utils.h
+ * @brief       Header file containing logging utility functions and definitions
  * @author      Leonardo Acha Boiano
  * @date        7 Jun 2023
  * 
  * @note        This code is written in C and is used on an ESP32-CAM development board.
  *
- *******************************************************************************/
+ ******************************************************************************/
 
 #ifndef LOGGING_UTILS_H
 #define LOGGING_UTILS_H
 
 #include <esp_log.h>
 
-// Tags for logging purposes
+/**
+ * @brief Tag for camera-related log messages
+ */
 extern const char *CAMERA_TAG;
+
+/**
+ * @brief Tag for webserver-related log messages
+ */
 extern const char *WEBSERVER_TAG;
+
+/**
+ * @brief Tag for admin-related log messages
+ */
 extern const char *ADMIN_TAG;
 
 #endif  // LOGGING_UTILS_H
