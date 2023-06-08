b8245e3bdfee61beeb3535dcd1f3429dff7e4621
Leonardo Achá <leonardoacha@hotmail.com>
2023-06-07 23:53:56 -0400

    http_handlers.h file documentation improved

--- src/device/main/src/http_handlers/http_handlers.h
+++ src/device/main/src/http_handlers/http_handlers.h
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * @file        http_handlers.h
+ * @brief       Header file containing declarations of HTTP request handlers.
  * @author      Leonardo Acha Boiano
  * @date        7 Jun 2023
  * 
@@ -22,11 +23,38 @@
 #include "../user_roles/user_roles.h"
 #include "../base64/base64_utils.h"
 
+/**
+ * @brief       HTTP request handler for getting a single image in base64 encoding.
+ * @param[in]   req The HTTP request object.
+ * @return      An esp_err_t indicating the success or failure of the operation.
+ */
 esp_err_t image_base64_httpd_handler(httpd_req_t *req);
+
+/**
+ * @brief       HTTP request handler for getting a single image.
+ * @param[in]   req The HTTP request object.
+ * @return      An esp_err_t indicating the success or failure of the operation.
+ */
 esp_err_t image_httpd_handler(httpd_req_t *req);
+
+/**
+ * @brief       HTTP request handler for getting the camera status.
+ * @param[in]   req The HTTP request object.
+ * @return      An esp_err_t indicating the success or failure of the operation.
+ */
 esp_err_t status_httpd_handler(httpd_req_t *req);
+
+/**
+ * @brief       HTTP request handler for admin functionality.
+ * @param[in]   req The HTTP request object.
+ * @return      An esp_err_t indicating the success or failure of the operation.
+ */
 esp_err_t admin_httpd_handler(httpd_req_t *req);
 
+/**
+ * @brief       The authenticated user role.
+ * @note        This variable is defined in main.c.
+ */
 extern UserRole authenticatedUserRole;
 
 #endif  // HTTP_HANDLERS_H
