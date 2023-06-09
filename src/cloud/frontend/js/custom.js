cc8eb0f60e2e67c7c92955f6fd918e1f98d38425
Andres Ayala <andresad7843@gmail.com>
2023-06-09 11:42:40 -0400

    Cambios en el index y statistics

--- src/cloud/frontend/js/custom.js
+++ src/cloud/frontend/js/custom.js
@@ -39,3 +39,18 @@ const darkModeToggle = document.getElementById('dark-mode-toggle');
     // Alterna la clase 'dark-mode' en el elemento raíz
     rootElement.classList.toggle('dark-mode');
   });
+      function mostrarFechaActual() {
+      var fecha = new Date(); // Obtiene la fecha actual
+      var dia = fecha.getDate(); // Obtiene el día del mes
+      var mes = fecha.getMonth() + 1; // Obtiene el mes (los meses comienzan en 0, por eso se le suma 1)
+      var anio = fecha.getFullYear(); // Obtiene el año
+
+      // Formatea la fecha en el formato deseado (por ejemplo, dd/mm/yyyy)
+      var fechaFormateada = dia + '/' + mes + '/' + anio;
+
+      // Inserta la fecha en el elemento con el id "fecha"
+      document.getElementById('fecha').innerHTML = fechaFormateada;
+    }
+
+    // Llama a la función mostrarFechaActual al cargar la página
+    window.onload = mostrarFechaActual;
