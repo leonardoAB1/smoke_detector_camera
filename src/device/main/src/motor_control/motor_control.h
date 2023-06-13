b15464b909b3719332b2a6cde20a8d57e78deba3
Leonardo Achá <leonardoacha@hotmail.com>
2023-06-13 10:24:27 -0400

    Redocumented motor_control.h for admin user permissions and consistent motor naming.

--- src/device/main/src/motor_control/motor_control.h
+++ src/device/main/src/motor_control/motor_control.h
@@ -11,12 +11,33 @@
 
 #include <stdbool.h>
 
-#define MOTOR_01 1
-#define MOTOR_02 2
+#define MOTOR_1 1
+#define MOTOR_2 2
 
 #define MOTOR_FREQUENCY 50
 
+/**
+ * @brief Moves the specified motor to a custom angle.
+ * 
+ * @param motor The motor to move (MOTOR_1 or MOTOR_2).
+ * @param angle The desired angle for the motor (0 to 180 for MOTOR_1, 0 to 90 for MOTOR_2).
+ * 
+ * @note This function allows admins to control the motors freely through a REST API command.
+ *       The motor angle is updated as requested and no longer follows the default pattern.
+ * @note Admin user permissions are required to execute this function.
+ */
 void move_motor_custom(int motor, int angle);
+
+/**
+ * @brief Moves the specified motor to the default angle.
+ * 
+ * @param motor The motor to move (MOTOR_1 or MOTOR_2).
+ * 
+ * @note This function returns the specified motor to the default angle as per the default pattern.
+ *       The default pattern consists of motor1 moving from 0 to 180 degrees, with each degree taking 2 seconds,
+ *       and motor2 moving from 0 to 90 degrees, with each degree taking 3 seconds.
+ * @note Admin user permissions are required to stop and restart this function.
+ */
 void move_motor_default(int motor);
 
 #endif /* MOTOR_CONTROL_H_ */
